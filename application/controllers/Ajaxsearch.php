7c81993188f285b8a8bc03ef5b521e8e0c2a2d49
Dana Sugu <dana.sugu@gmail.com>
2021-10-29 11:17:05 +0300

    update

--- application/controllers/Ajaxsearch.php
+++ application/controllers/Ajaxsearch.php
@@ -23,8 +23,9 @@ class Ajaxsearch extends CI_Controller {
   <div class="table-responsive">
      <table class="table table-bordered table-striped">
       <tr>
-       <th>Customer Name</th>
+       <th>Contact Name</th>
        <th>Address</th>
+       <th>Email</th>
        <th>City</th>
        <th>Postal Code</th>
        <th>Country</th>
@@ -36,8 +37,9 @@ class Ajaxsearch extends CI_Controller {
    {
     $output .= '
       <tr>
-       <td>'.$row->CustomerName.'</td>
+       <td>'.$row->ContactName.'</td>
        <td>'.$row->Address.'</td>
+       <td>'.$row->Email.'</td>
        <td>'.$row->City.'</td>
        <td>'.$row->PostalCode.'</td>
        <td>'.$row->Country.'</td>
--- application/models/Ajaxsearch_model.php
+++ application/models/Ajaxsearch_model.php
@@ -4,16 +4,17 @@ class Ajaxsearch_model extends CI_Model
  function fetch_data($query)
  {
   $this->db->select("*");
-  $this->db->from("tbl_customer");
+  $this->db->from("tbl_contacts");
   if($query != '')
   {
-   $this->db->like('CustomerName', $query);
+   $this->db->like('ContactName', $query);
    $this->db->or_like('Address', $query);
+   $this->db->or_like('Email', $query);
    $this->db->or_like('City', $query);
    $this->db->or_like('PostalCode', $query);
    $this->db->or_like('Country', $query);
   }
-  $this->db->order_by('CustomerID', 'DESC');
+  $this->db->order_by('ContactID', 'DESC');
   return $this->db->get();
  }
 }
