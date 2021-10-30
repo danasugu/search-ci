f8777c6179117c9e81430dad03a14540cb1f1431
Dana Sugu <dana.sugu@gmail.com>
2021-10-30 19:04:10 +0300

    Create users_view.php

---
+++ application/views/users_view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <meta charset="utf-8">
+   <title> CodeIgniter EXPORT</title>
+ </head>
+ <body>
+   <!-- Export Data -->
+   <a href='<?= base_url() ?>crud/export_csv'>Export</a><br><br>
+
+   <!-- User Records -->
+   <table border='1' style='border-collapse: collapse;'>
+     <thead>
+      <tr>
+       <th>Username</th>
+       <th>Name</th>
+       <th>Gender</th>
+       <th>Email</th>
+      </tr>
+     </thead>
+     <tbody>
+     <?php
+     foreach($usersData as $key=>$val){
+       echo "<tr>";
+       echo "<td>".$val['username']."</td>";
+       echo "<td>".$val['name']."</td>";
+       echo "<td>".$val['gender']."</td>";
+       echo "<td>".$val['email']."</td>";
+       echo "</tr>";
+      }
+      ?>
+     </tbody>
+    </table>
+  </body>
+</html>
